318208cf8ed6649c60c4f641a0f8e4041642afdb
fans-dev <fnassiir22@gmail.com>
2020-04-12 00:31:00 +0300

    Feat Fansmail login
    What does PR do?
    -Add user Email address
    -Add user password
    -Log in user to access other pages
    -Authenticate user
    
    How to test it?
    -click in the index.html file
    -Add user name
    -Add password
    -Create account if don't have one
    -click signin to proceed
    
    Built with
    -Html
    -CSS
    -Javascript Fetch-API

---
+++ signin.js
@@ -0,0 +1,53 @@
+// const login = document.getElementById('loginbtn');
+
+// login.addEventlistner('click', function(e)
+// {
+//     e.preventDefault();
+//  });
+ function signin(){
+
+    const email_address = document.getElementById("email_add");
+    const  password = document.getElementById("password");
+    
+    
+   const url ="https://epicmail-sentongo-v2.herokuapp.com/api/v2/auth/signin";
+       
+    
+        const signin_user = {
+            "email_address": email_add.value,
+            "password": password.value,
+      
+    };
+    
+    fetch(url, {
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        
+        body: JSON.stringify(signin_user),
+            "email_address": email_address.value,
+            "password": password.value,
+            
+        
+    })
+        .then((response) => response.json())
+        .then((data) => {
+                if (data.status !== 200) {
+                document.getElementById("response").style.display ="block";
+                document.getElementById("response").style.color ="red";
+                document.getElementById("response").innerHTML = data.error;
+                
+            }
+            else if (data.status === 200){
+                document.getElementById("response").style.display = "block";
+                document.getElementById("response").style.color = "green"
+                document.getElementById("response").innerHTML = "signed in Successfuly"
+                window.location.replace("dashboard.html");
+            
+    
+            }
+        
+    }).catch(error => { console.log("error",error)});
+    }
+    
